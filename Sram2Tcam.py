4e520661a5dfd587dd325ad03858f3705b64a15a
chwang <wangdc1111@hotmail.com>
2024-11-04 20:20:57 +0800

    add a python script for coverting match.txt back to tcam files

---
+++ Sram2Tcam.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import sys
+import os
+import re
+
+c4_c2_map = {
+    "1000": "00",
+    "0100": "01",
+    "0010": "10",
+    "0001": "11",
+    "1100": "0x",
+    "1010": "x0",
+    "0011": "1x",
+    "0101": "x1",
+    "1111": "xx",
+}
+
+TCAM_ENTRY_CNT = 32
+MAX_ENTRY_NO = TCAM_ENTRY_CNT - 1
+TOTAL_STATE_CNT = 32
+BIT_CNT_PER_STATE_PER_RAM = 4
+TOTAL_BIT_CNT_PER_RAM = TOTAL_STATE_CNT * BIT_CNT_PER_STATE_PER_RAM
+TOTAL_RAM_CHIP_CNT = 20
+TOTAL_RAM_LINE_CNT = TOTAL_BIT_CNT_PER_RAM * TOTAL_RAM_CHIP_CNT
+
+if len(sys.argv) != 2:
+    print("this script shall take exactly one argument of input path to source file!")
+    exit(1)
+
+in_fpath = sys.argv[1]
+if not os.path.exists(in_fpath):
+    print("the input path to source file does not exist!")
+    exit(1)
+
+# in_fname = os.path.basename(in_fpath)
+data = []
+with open(in_fpath, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for line in lines:
+        if re.match(r'^\s*\/\/.*$', line.strip()):
+            continue
+        if not re.match(r'^[01]{32}$', line.strip()):
+            exit(1)
+        data.append(line.strip())
+
+if len(data) != TOTAL_RAM_LINE_CNT:
+    print("input data file wrong!")
+    exit(1)
+
+for column in reversed(range(TCAM_ENTRY_CNT)):
+    trans_data = ''.join([row[column] for row in data])
+    for j in range(0, TOTAL_BIT_CNT_PER_RAM, BIT_CNT_PER_STATE_PER_RAM):
+        if not len(os.path.dirname(in_fpath)):
+            ot_path = os.path.curdir + "/" + str(j // 4).rjust(3, '0') + ".tcam"
+        else:
+            ot_path = os.path.dirname(in_fpath) + "/" + str(j // 4).rjust(3, '0') + ".tcam"
+        if column == MAX_ENTRY_NO:
+            file = open(ot_path, 'w', encoding='utf-8')
+        else:
+            file = open(ot_path, 'a', encoding='utf-8')
+        entry = []
+        for i in range(20):
+            start = i * TOTAL_BIT_CNT_PER_RAM + j
+            substr = trans_data[start : start + 4]
+            entry.append(c4_c2_map[substr])
+        entry.reverse()
+        file.write(''.join(entry) + "\n")
