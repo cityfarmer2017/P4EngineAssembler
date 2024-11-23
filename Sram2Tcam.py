f6ca0684f10c92507ba5bbfc16c2ac4f8eab5b6a
wangdianchao <wangdianchao@ehtcn.com>
2024-11-23 10:23:47 +0800

    seperated python scripting code into pieces to make it more readable

--- Sram2Tcam.py
+++ Sram2Tcam.py
@@ -24,40 +24,53 @@ TOTAL_BIT_CNT_PER_RAM = TOTAL_STATE_CNT * BIT_CNT_PER_STATE_PER_RAM
 TOTAL_RAM_CHIP_CNT = 20
 TOTAL_RAM_LINE_CNT = TOTAL_BIT_CNT_PER_RAM * TOTAL_RAM_CHIP_CNT
 
-if len(sys.argv) != 2:
-    print("this script shall take exactly one argument of input path to source file!")
-    exit(1)
+def check_parameters_valid() -> None:
+    if len(sys.argv) != 2:
+        print("this script shall take exactly one argument of input path to source file!")
+        exit(1)
 
-in_fpath = sys.argv[1]
-if not os.path.exists(in_fpath):
-    print("the input path to source file does not exist!")
-    exit(1)
+def get_input_file_path() -> str:
+    if not os.path.exists(sys.argv[1]):
+        print("the input path to source file does not exist!")
+        exit(1)
+    return sys.argv[1]
 
-# in_fname = os.path.basename(in_fpath)
-data = []
-with open(in_fpath, 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        if re.match(r'^\s*\/\/.*$', line.strip()):
-            continue
-        if not re.match(r'^[01]{32}$', line.strip()):
-            exit(1)
-        data.append(line.strip())
+def get_input_data_list(path: str) -> list[str]:
+    data = []
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if re.match(r'^\s*\/\/.*$', line.strip()):
+                continue
+            if not re.match(r'^[01]{32}$', line.strip()):
+                exit(1)
+            data.append(line.strip())
+    if len(data) != TOTAL_RAM_LINE_CNT:
+        print("input data file wrong!")
+        exit(1)
+    return data
 
-if len(data) != TOTAL_RAM_LINE_CNT:
-    print("input data file wrong!")
-    exit(1)
+def transform_sram_2_tcam(data: list[str], path: str) -> None:
+    for column in reversed(range(TCAM_ENTRY_CNT)):
+        trans_data = ''.join([row[column] for row in data])
+        for state_id in range(0, TOTAL_BIT_CNT_PER_RAM, BIT_CNT_PER_STATE_PER_RAM):
+            ot_path = os.path.dirname(path) if len(os.path.dirname(path)) else os.path.curdir
+            ot_path += "/" + str(state_id // BIT_CNT_PER_STATE_PER_RAM).rjust(3, '0') + ".tcam"
+            file = open(ot_path, 'w') if column == MAX_ENTRY_NO else open(ot_path, 'a')
+            entry = []
+            for ram_id in range(TOTAL_RAM_CHIP_CNT):
+                start = ram_id * TOTAL_BIT_CNT_PER_RAM + state_id
+                substr = trans_data[start : start + BIT_CNT_PER_STATE_PER_RAM]
+                entry.append(c4_c2_map[substr])
+            entry.reverse()
+            file.write(''.join(entry) + "\n")
 
-for column in reversed(range(TCAM_ENTRY_CNT)):
-    trans_data = ''.join([row[column] for row in data])
-    for state_id in range(0, TOTAL_BIT_CNT_PER_RAM, BIT_CNT_PER_STATE_PER_RAM):
-        ot_path = os.path.dirname(in_fpath) if len(os.path.dirname(in_fpath)) else os.path.curdir
-        ot_path += "/" + str(state_id // BIT_CNT_PER_STATE_PER_RAM).rjust(3, '0') + ".tcam"
-        file = open(ot_path, 'w') if column == MAX_ENTRY_NO else open(ot_path, 'a')
-        entry = []
-        for ram_id in range(TOTAL_RAM_CHIP_CNT):
-            start = ram_id * TOTAL_BIT_CNT_PER_RAM + state_id
-            substr = trans_data[start : start + BIT_CNT_PER_STATE_PER_RAM]
-            entry.append(c4_c2_map[substr])
-        entry.reverse()
-        file.write(''.join(entry) + "\n")
+def main() -> None:
+    check_parameters_valid()
+    in_fpath: str = get_input_file_path()
+    # in_fname = os.path.basename(in_fpath)
+    data: list[str] = get_input_data_list(in_fpath)
+    transform_sram_2_tcam(data, in_fpath)
+
+if __name__ == '__main__':
+    main()
